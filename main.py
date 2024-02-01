9399b01fcc8792675612e38f23a165d071f9fb35
siva <rsiva.tnj@gmail.comcls>
2024-02-01 23:40:55 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from src.mlproject import logger
 
-logger.info("Welcome to our custom logging")
+from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started >>>>>>")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed >>>>>\n\nx=========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
---
+++ src/mlproject/entity/config_entity.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen= True)
+class DataIngestionConfig:        # written type : told my method to  return these four variables
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+   
+    unzip_dir: Path
+
+
+
+
+
+
+
+
+
+
+@dataclass(frozen = True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
